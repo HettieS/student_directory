cedded0dc500053df070e9bf990478e2891601a0
Hettie Street <hettiestreet@Hetties-MacBook-Pro.local>
2022-01-06 12:56:00 +0000

    adding methods

--- directory.rb
+++ directory.rb
@@ -9,13 +9,21 @@ students = [
     "Buddy the elf"
 ]
 
-# we print the list of names
-puts "The students of Christmas Academy"
-puts "--------------------"
-
-students.each do |student|
-    puts student
+def print_header
+    puts "The students of Christmas Academy"
+    puts "--------------------"
 end
 
-# then we print the total students
-puts "Overall we have #{students.count} great students."
+def print(names)
+    names.each do |name|
+        puts name
+    end
+end
+
+def print_footer(names)
+    puts "Overall we have #{names.count} great students."
+end
+
+print_header
+print(students)
+print_footer(students)
